d9fc6c7603c9b3c1f63a9a82d68512d6d0f1a09b
张新龙 <173647085@qq.com>
2025-06-25 15:50:14 +0800

    fix: remove Flask dependency

--- server/api/handler.py
+++ server/api/handler.py
@@ -1,15 +1,3 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return "Hello from Vercel!"
-
-@app.route('/api/test', methods=['GET'])
-def test():
-    return "API is working!"
-
 def handler(request):
     """
     Vercel serverless function handler
